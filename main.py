a0fd63f96bdc932c25ba91db6092af4d78065cfc
Talareto <99801682+Talareto@users.noreply.github.com>
2024-10-13 21:48:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-import string
+import re
 from collections import defaultdict
 
 def wyczysc_tekst(tekst):
-    return tekst.translate(str.maketrans('', '', string.punctuation)).lower().split()
+    return re.findall(r'\b\w+\b', tekst.lower())
 
 liczba_dokumentow = int(input())
 dokumenty = [input().strip() for _ in range(liczba_dokumentow)]
 liczba_slow = int(input())
 slowa = [input().strip().lower() for _ in range(liczba_slow)]
 
-# Inicjalizacja słownika do przechowywania wyników
-wyniki = defaultdict(list)
+# Inicjalizacja słownika do przechowywania wystąpień słów w dokumentach
+word_occurrences = defaultdict(lambda: defaultdict(int))
 
+# Zliczanie wystąpień słów w dokumentach
 for index, dokument in enumerate(dokumenty):
     oczyszczony_dokument = wyczysc_tekst(dokument)
-    zliczanie = defaultdict(int)
-
     for slowo in oczyszczony_dokument:
-        if slowo in slowa:
-            zliczanie[slowo] += 1
+        word_occurrences[slowo][index + 1] += 1  # Indeks dokumentu od 1
 
-    for slowo in slowa:
-        if zliczanie[slowo] > 0:
-            wyniki[slowo].append(index + 1)  # Dodajemy 1, żeby indeks był od 1
+# Przygotowanie wyników
+wyniki_listy = []
+
+for slowo in slowa:
+    if slowo in word_occurrences:
+        occurrences = sorted(word_occurrences[slowo].items(), key=lambda x: (-x[1], x[0]))
+        wyniki_listy.append([doc_id for doc_id, count in occurrences])
+    else:
+        wyniki_listy.append([])
 
 # Wyświetlenie wyników
-for slowo in slowa:
-    print(" ".join(map(str, wyniki[slowo])))
+for lista in wyniki_listy:
+    print(" ".join(map(str, lista)))
