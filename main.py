8c27fcf8cd72eb425d03905b276e8fb52150445b
Talareto <99801682+Talareto@users.noreply.github.com>
2024-10-13 21:39:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 from collections import defaultdict
 
-def process_documents_and_queries(documents,queries):
-  word_in_docs = defaultdict(lambda:defaultdict(int))
+def process_documents_and_queries(documents, queries):
+    word_in_docs = defaultdict(lambda: defaultdict(int))
 
-  for doc_index, document in enumerate(documents):
-    for word in document.split():
-      word_in_docs[word][doc_index] += 1
+    for doc_index, document in enumerate(documents):
+        for word in document.lower().split():
+            word_in_docs[word][doc_index] += 1
 
-  results = []
+    results = []
+    for query in queries:
+        query = query.lower()
+        if query in word_in_docs:
+            occurrences = list(word_in_docs[query].items())
+            sorted_occurrences = sorted(occurrences, key=lambda x: (-x[1], x[0]))
+            results.append([doc_index for doc_index, _ in sorted_occurrences])
+        else:
+            results.append([])
 
-  for query in queries:
-    if query in word_in_docs:
-      occurrences = list(word_in_docs[query].items())
-      sorted_occurrences = sorted(occurrences, key = lambda x: (-x[1], x[0]))
-      results.append([doc_index for doc_index, _ in sorted_occurrences])
-    else:
-      results.append([])
-
-  return results
+    return results
 
 n = int(input())
 documents = [input() for _ in range(n)]
+
 m = int(input())
 queries = [input() for _ in range(m)]
 
